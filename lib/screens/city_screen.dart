e58cce89cc78fe6a4b2b36335c392807f3017f35
Anurag Kainth <anuragkainth07@gmail.com>
2023-04-04 14:16:41 +0530

    adding wallpapers

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -15,7 +15,7 @@ class _CityScreenState extends State<CityScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
+            image: AssetImage('images/wallpaper_3.jpg'),
             fit: BoxFit.cover,
           ),
         ),
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -52,7 +52,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/wallpaper_1.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
